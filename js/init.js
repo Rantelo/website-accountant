d762b3a829c7ea842daba7b744de04c26ff0fc42
Roberto Antelo <roberto_antelo@hotmail.com>
2016-09-02 02:08:53 -0500

    Add rough implementation of google forms into contact form

--- js/init.js
+++ js/init.js
@@ -1,8 +1,11 @@
+var submitted= false;
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('#gform').on('submit', function(e) {
+      $('#gform').prepend('Your submission has been processed...');
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
